8a65f45577ba7efae423bd97d1ba8f1a1f09507e
Yaroslav Stasenko <87314914+YaroslavV1@users.noreply.github.com>
2023-04-20 11:18:34 +0300

    Implement builder pattern #1
    feat - Implemented builder pattern.

---
+++ Builder/Builders/ManualMonitorBuilder.cs
@@ -0,0 +1,55 @@
+﻿using Builder.Builders.Interfaces;
+using Monitor = Builder.Entities.Monitor;
+
+namespace Builder.Builders;
+
+public class ManualMonitorBuilder : IMonitorBuilder
+{
+    private const string MATRIX = "VA";
+    private const string DIAGONAL = "19";
+    private const string SCREENRESOLUTION = "1600x900";
+    private const string UPDATEFREQUENCY = "5ms";
+    private const string CONTRAST = "1000:1";
+    
+    private readonly Monitor _monitor;
+
+    public ManualMonitorBuilder()
+    {
+        _monitor = new Monitor();
+    }
+    
+    public IMonitorBuilder BuildMatrix()
+    {
+        _monitor.Matrix = MATRIX;
+        return this;
+    }
+
+    public IMonitorBuilder BuildDiagonal()
+    {
+        _monitor.Diagonal = DIAGONAL;
+        return this;
+    }
+
+    public IMonitorBuilder BuildScreenResolution()
+    {
+        _monitor.ScreenResolution = SCREENRESOLUTION;
+        return this;
+    }
+
+    public IMonitorBuilder BuildUpdateFrequency()
+    {
+        _monitor.UpdateFrequency = UPDATEFREQUENCY;
+        return this;
+    }
+
+    public IMonitorBuilder BuildContrast()
+    {
+        _monitor.Contrast = CONTRAST;
+        return this;
+    }
+
+    public Monitor GetMonitor()
+    {
+        return _monitor;
+    }
+}
---
+++ Builder/Builders/OfficeMonitorBuilder.cs
@@ -0,0 +1,55 @@
+﻿using Builder.Builders.Interfaces;
+using Monitor = Builder.Entities.Monitor;
+
+namespace Builder.Builders;
+
+public class OfficeMonitorBuilder : IMonitorBuilder
+{
+    private const string MATRIX = "VA";
+    private const string DIAGONAL = "22";
+    private const string SCREENRESOLUTION = "1920x1080";
+    private const string UPDATEFREQUENCY = "5ms";
+    private const string CONTRAST = "1000:1";
+    
+    private readonly Monitor _monitor;
+
+    public OfficeMonitorBuilder()
+    {
+        _monitor = new Monitor();
+    }
+    
+    public IMonitorBuilder BuildMatrix()
+    {
+        _monitor.Matrix = MATRIX;
+        return this;
+    }
+
+    public IMonitorBuilder BuildDiagonal()
+    {
+        _monitor.Diagonal = DIAGONAL;
+        return this;
+    }
+
+    public IMonitorBuilder BuildScreenResolution()
+    {
+        _monitor.ScreenResolution = SCREENRESOLUTION;
+        return this;
+    }
+
+    public IMonitorBuilder BuildUpdateFrequency()
+    {
+        _monitor.UpdateFrequency = UPDATEFREQUENCY;
+        return this;
+    }
+
+    public IMonitorBuilder BuildContrast()
+    {
+        _monitor.Contrast = CONTRAST;
+        return this;
+    }
+
+    public Monitor GetMonitor()
+    {
+        return _monitor;
+    }
+}
