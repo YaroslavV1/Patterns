4ad283f2c291aa26f6621b1028631a4804405715
Yaroslav Stasenko <87314914+YaroslavV1@users.noreply.github.com>
2023-04-20 11:31:05 +0300

    Implement Strategy pattern #3
    feat - Implemented Strategy pattern.

---
+++ Strategy/Models/Oven.cs
@@ -0,0 +1,16 @@
+﻿using Strategy.Abstracts;
+
+namespace Strategy.Models;
+
+public class Oven : CookingEquipment
+{
+    public Oven()
+    {
+        Name = "Oven";
+    }
+    
+    public override void Cook()
+    {
+        Console.WriteLine($"Cooking with a {Name}.");
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Channels;
+using Strategy.Models;
+
+var chef = new Chef();
+
+Console.WriteLine("\n\tStarting program!");
+
+chef.SetCookingEquipment(new Stove());
+chef.Cook();
+
+Console.WriteLine();
+
+chef.SetCookingEquipment(new Oven());
+chef.Cook();
+
+Console.WriteLine("\n\tEnd program!");
